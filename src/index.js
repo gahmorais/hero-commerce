ffba12e8b414c64af79a66a383bad6d89d66f35b
Gabriel Morais <gmdeveloper01@gmail.com>
2022-04-23 08:57:35 -0300

    task: add port from environment

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Api imobiliaria" });
 });
 
-app.listen("3000", () => {
+app.listen(process.env.PORT || "3000", () => {
   console.log("API online");
 });
