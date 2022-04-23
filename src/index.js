f1b53a041de959080792bd021c06f0d03cf937f7
Gabriel Morais <gmdeveloper01@gmail.com>
2022-04-23 09:36:59 -0300

    fixed: config sequelize database env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// require("dotenv/config");
 const app = require("./server");
-// const clientRoute = require("./routes/clientRoute");
-// const propertyRoute = require("./routes/propertyRoute");
-// const realtorRoute = require("./routes/realtorRoute");
+const clientRoute = require("./routes/clientRoute");
+const propertyRoute = require("./routes/propertyRoute");
+const realtorRoute = require("./routes/realtorRoute");
 
-// app.use(clientRoute);
-// app.use(propertyRoute);
-// app.use(realtorRoute);
+app.use(clientRoute);
+app.use(propertyRoute);
+app.use(realtorRoute);
 
 app.get("/", (req, res) => {
   res.json({ message: "Api imobiliaria" });
